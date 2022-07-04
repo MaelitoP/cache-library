875aae729cc41e391b47f9ed29170605d68dc047
Maël LE PETIT <makibeardy@gmail.com>
2022-07-04 12:05:09 +0200

    Update CacheInterface lisibility

--- src/Model/CacheInterface.php
+++ src/Model/CacheInterface.php
@@ -4,12 +4,15 @@ namespace Mention\Cache\Model;
 interface CacheInterface
 {
     /**
-     * Store data in memory
+     * Store data in cache
+     * @param string $key
+     * @param $value
      */
     public function set(string $key, $value): void;
 
     /**
-     * Get data from memory
+     * Get data from cache
+     * @param string $key
      * @return mixed - value on success or null on failure
      */
     public function get(string $key): mixed;
